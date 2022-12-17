d8fd1b0a01ff0ace998315cff15e0080931829f2
dibakar63 <110836157+dibakar63@users.noreply.github.com>
2022-12-17 20:49:54 +0530

    Add files via upload

---
+++ src/components/Employee.js
@@ -0,0 +1,17 @@
+const Employees=[
+    {
+        id:"1",
+        Name:"Dibakar Dey",
+        Age:"30"
+    },{
+        id:"2",
+        Name:"Sankhasubhra Das",
+        Age:"28",
+    },{
+        id:"3",
+        Name:"Arnab Sil",
+        Age:"32"
+    }
+]
+
+export default Employees;
